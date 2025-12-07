89c18979860c7b5a9712aca5fc661248de33f058
Новинский Денис <waver736@gmail.com>
2025-12-07 19:29:34 +1000

    feat(gallery): Добавлена галерея с фото пользователей

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>Hello World!</div>
+  <Gallery :images="photos" />
 </template>
 
 <style scoped>
@@ -9,3 +8,20 @@ div {
   font-family: var(--font-main);
 }
 </style>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+  import Gallery from "./components/Gallery.vue";
+
+  const photos = ref([
+    "images/uzbek1.png",
+    "images/uzbek2.png",
+    "images/uzbek3.png",
+    "images/uzbek4.png",
+    "images/uzbek5.png",
+    "images/uzbek6.png",
+    "images/uzbek7.png",
+    "images/uzbek8.png",
+    "images/uzbek9.png",
+  ]);
+</script>
