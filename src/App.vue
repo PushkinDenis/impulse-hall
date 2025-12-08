f9ba779b1b962c0cf6f7f6779c9f70c24c5c29dd
Denis <denisp9831@gmail.com>
2025-12-08 16:02:35 +1000

    Create Header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,5 @@
-<script setup lang="ts">
-import Button from './Button.vue';
-</script>
+<script setup lang="ts"></script>
 
-<template>
-  <div class="container">
-    <div>cscsc</div>
-    <Button>{{ 'Получить консультацию' }}</Button>
-    <Button color="orange">{{ 'Получить консультацию' }}</Button>
-    <Button variant="custom">{{ 'Получить консультацию' }}</Button>
-    <Button>{{ 'Зарегестрироваться' }}</Button>
-  </div>
-</template>
+<template></template>
 
-<style scoped>
-.container {
-  display: flex;
-  height: 100vh;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+<style scoped></style>
