b0fb096ad40e58c72506cfc9a6446424a20e908a
Pushkin Denis <105781316+PushkinDenis@users.noreply.github.com>
2025-12-08 19:42:09 +1000

    Create MainPage

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <script setup lang="ts">
-  import Button from './Button.vue';
-  import DescriptionPage from "./components/DescriptionPage/DescriptionPage.vue";
-  import MembershipPage from "./components/MembershipPage/MembershipPage.vue";
+import DescriptionPage from './components/DescriptionPage/DescriptionPage.vue';
+import MembershipPage from './components/MembershipPage/MembershipPage.vue';
+import MainPage from './components/MainPage/MainPage.vue';
 </script>
 
 <template>
-  <div class="container">
-    <div>cscsc</div>
-    <Button>{{ 'Получить консультацию' }}</Button>
-    <Button color="orange">{{ 'Получить консультацию' }}</Button>
-    <Button variant="custom">{{ 'Получить консультацию' }}</Button>
-    <Button>{{ 'Зарегестрироваться' }}</Button>
-  </div>
-  <DescriptionPage/>
-  <MembershipPage/>
+  <MainPage />
+  <DescriptionPage />
+  <MembershipPage />
 </template>
 
 <style scoped></style>
---
+++ src/components/MainPage/MainPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-page-cont">
+    <Header />
+    <MainDescription />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from '../../Header.vue';
+import MainDescription from './layout/MainDescription.vue';
+defineOptions({ name: 'MainPage' });
+</script>
+
+<style scoped>
+.main-page-cont {
+  height: 100vh;
+  background-image: url('/images/cover.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
